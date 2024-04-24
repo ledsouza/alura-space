880fc997da7a085e966eb3ca220306b37502a6fe
ledsouza <leandro.souza.159@gmail.com>
2024-04-24 20:33:56 -0300

    Adicionado um novo campo a Fotografia

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class FotografiaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'categoria')
     list_display_links = ('id', 'nome')
-    list_filter = ('nome',)
+    list_filter = ('categoria',)
     search_fields = ('nome', 'legenda')
 
 admin.site.register(Fotografia, FotografiaAdmin)
