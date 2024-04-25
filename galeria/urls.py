d361dacca38daea1e86d9d30a985ca5ce484ec0e
ledsouza <leandro.souza.159@gmail.com>
2024-04-25 08:53:06 -0300

    alterado o campo foto em fotografia

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from galeria.views import index, imagem
 
 urlpatterns = [
     path("", index, name="index"),
     path("imagem/<int:foto_id>", imagem, name="imagem")
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
