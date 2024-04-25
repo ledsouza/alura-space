0159976ba2c8c0aa52787547345616b083339192
ledsouza <leandro.souza.159@gmail.com>
2024-04-24 21:38:14 -0300

    adicionado campo publicada em fotografia

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,9 +3,11 @@ from django.contrib import admin
 from galeria.models import Fotografia
 
 class FotografiaAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'legenda', 'categoria')
+    list_display = ('id', 'nome', 'legenda', 'categoria', 'publicada')
     list_display_links = ('id', 'nome')
     list_filter = ('categoria',)
+    list_editable = ('publicada',)
     search_fields = ('nome', 'legenda')
+    list_per_page = 10
 
 admin.site.register(Fotografia, FotografiaAdmin)
