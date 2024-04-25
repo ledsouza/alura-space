e61a6d1ae770d17fa123968df300916f130a1676
ledsouza <leandro.souza.159@gmail.com>
2024-04-25 11:13:10 -0300

    configurada a pagina de busca

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 
 urlpatterns = [
     path("", index, name="index"),
-    path("imagem/<int:foto_id>", imagem, name="imagem")
+    path("imagem/<int:foto_id>", imagem, name="imagem"),
+    path("buscar", buscar, name="buscar")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
