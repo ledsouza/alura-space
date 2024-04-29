e99fa94b6276987b56212a00bff4b1b5023f1351
ledsouza <leandro.souza.159@gmail.com>
2024-04-29 16:35:39 -0300

    configurada a página de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,15 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 from usuarios.forms import LoginForm, CadastroForm
 
-def login(request):
+def login(request, sucesso=None):
     context = {
-        'form': LoginForm()
+        'form': LoginForm(),
+        'sucesso': sucesso
     }
     return render(request, 'usuarios/login.html', context)
 
 def cadastro(request):
+    form = CadastroForm(request.POST or None)
     context = {
-        'form': CadastroForm()
+        'form': form
     }
+
+    if form.is_valid():
+        if form.cleaned_data['senha_1'] != form.cleaned_data['senha_2']:
+            context['erro'] = 'As senhas não conferem'
+            redirect('cadastro')
+        
+        username = form.cleaned_data['nome_cadastro']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['senha_1']
+
+        if User.objects.filter(username=username).exists():
+            context['erro'] = 'Nome de usuário já cadastrado'
+            redirect('cadastro')
+
+        usuario = User.objects.create_user(username, email, password)
+        usuario.save()
+        return redirect('login', sucesso='Cadastro realizado com sucesso')
+
     return render(request, 'usuarios/cadastro.html', context)
