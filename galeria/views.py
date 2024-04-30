6ccc026e2ab0f339952cd37a1549bdbe6886f399
ledsouza <leandro.souza.159@gmail.com>
2024-04-30 10:57:21 -0300

    corrigido bug de conflito no css

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+
 from galeria.models import Fotografia
 
 def index(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Você precisa estar logado para acessar essa página.")
+        return redirect("login")
+
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
     return render(request, "galeria/index.html", {"cards": fotografias})
 
@@ -10,6 +16,10 @@ def imagem(request, foto_id):
     return render(request, "galeria/imagem.html", {"fotografia": fotografia})
 
 def buscar(request):
+    if not request.user.is_authenticated:
+        messages.error(request, "Você precisa estar logado para acessar essa página.")
+        return redirect("login")
+    
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicada=True)
 
     if "buscar" in request.GET:
