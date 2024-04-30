e8d59a4816387b731763b49c44f655e53fbb41dc
ledsouza <leandro.souza.159@gmail.com>
2024-04-30 12:25:48 -0300

    definida uma partial para alertas

--- setup/settings.py
+++ setup/settings.py
@@ -137,3 +137,12 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: 'info',
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
