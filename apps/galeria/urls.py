65fb2c340b90e1e50b7f8b7be362ec412d19d928
ledsouza <leandro.souza.159@gmail.com>
2024-04-30 15:48:20 -0300

    adicionada FotografiaForms em forms.py

--- apps/galeria/urls.py
+++ apps/galeria/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from apps.galeria.views import index, imagem, buscar
+from apps.galeria import views
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("imagem/<int:foto_id>", imagem, name="imagem"),
-    path("buscar", buscar, name="buscar")
+    path("", views.index, name="index"),
+    path("imagem/<int:foto_id>", views.imagem, name="imagem"),
+    path("buscar", views.buscar, name="buscar"),
+    path("nova-imagem", views.nova_imagem, name="nova_imagem"),
+    path("editar-imagem", views.editar_imagem, name="editar_imagem"),
+    path("remover-imagem", views.remover_imagem, name="remover_imagem"),
 ] 
